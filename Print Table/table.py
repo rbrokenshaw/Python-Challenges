3d86d5d44785505b42bb3397190d9e56f1936086
rbrokenshaw <rbrokenshaw@gmail.com>
2019-10-30 17:20:03 +0000

    update to print table

--- Print Table/table.py
+++ Print Table/table.py
@@ -1,9 +1,18 @@
 def table(n):
+	output_list = []
 	for i in range(1,11):
-		print (n * i, end=" ")
+		output_list.append(n * i)
+	output = str(output_list).strip('[]')
+	output = output.replace(",", "")
+	return output
 
+
+print(table(4))
+
+'''
 if __name__ == "__main__":
 	t=int(input())
 	for i in range(t):
 		n = int(input())
 		print (table(n))
+'''
